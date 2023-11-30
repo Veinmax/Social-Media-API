4bb8a51fdd1aa6b4f70499e9ec65fc34ad2e6321
Veinmax <arturjust88@gmail.com>
2023-11-30 18:34:10 +0200

    remove unnecessary imports

--- social_media/views.py
+++ social_media/views.py
@@ -1,10 +1,7 @@
-from django.conf import settings
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from social_media.permissions import (
